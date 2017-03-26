f51879db5f33507a920fa4f5dfdea4c6b2fb829b
sijane <SJ@SJs-Mac.local>
2017-03-26 14:36:31 +0800

    Completed the modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,14 @@ import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import $ from 'jquery'
 import StickyHeader from './modules/StickyHeader'
+import Modal from './modules/Modal'
 
 const mobileMenu = new MobileMenu()
+
 new RevealOnScroll($('.feature-item'), '90%')
 // new RevealOnScroll(document.querySelectorAll('.feature-item'), '90%')
 new RevealOnScroll($('.testimonial'), '70%')
+
 const stickyHeader = new StickyHeader()
+
+const modal = new Modal()
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,35 @@
+import $ from 'jquery'
+
+class Modal {
+  constructor() {
+    this.openModalButton = $('.open-modal')
+    this.modal = $('.modal')
+    this.closeModalButton = $('.modal__close')
+    this.events()
+  }
+
+  events() {
+    this.openModalButton.click(this.openModal.bind(this))
+    this.closeModalButton.click(this.closeModal.bind(this))
+
+    $(document).keyup(this.keyPressHandler.bind(this))
+  }
+
+  keyPressHandler(e) {
+    if (e.keyCode == 27) {  // .keyCode or .which
+      this.closeModal()
+    }
+  }
+
+  openModal() {
+    this.modal.addClass('modal--is-visible')
+    return false;  // to be inactive to "#" behavior
+  }
+
+  closeModal() {
+    this.modal.removeClass('modal--is-visible')
+  }
+
+}
+
+export default Modal
